8783704d0cf7a58f961eb898c0d68735014cecb3
Maksim Palchevski <83406787+YraBot@users.noreply.github.com>
2024-06-14 16:52:07 +0300

    Meny developped

--- Assets/scripts/meny/SetLanguage.cs
+++ Assets/scripts/meny/SetLanguage.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class SetLanguage : MonoBehaviour
 {
     public byte language = 0;
+    public Transform flag;
     public void Set()
     {
         GeneralInfo.language = language;
+        flag.position = transform.position;
+    }
+    private void Awake()
+    {
+        if(GeneralInfo.language == language) flag.position = transform.position;
     }
 }
